4ae35d8b72c672d4378838b0a6091289b2fe3846
Peter-32 <peterjmyers1@gmail.com>
2026-09-06 15:03:58 -0600

    save

--- src/4_benchmark_injects.py
+++ src/4_benchmark_injects.py
@@ -1,34 +1,51 @@
 import pandas as pd
 from utils import *
 import matplotlib.pyplot as plt
+
 pro_df = local.read.csv('2_extract_pro_injects', 'interim')
-
-# We will skip very short games for benchmarking analysis 
 pro_df = pro_df.query("game_length_seconds_max_600 >= 420")
+my_df = local.read.csv('3_extract_my_injects', 'interim')
+my_df = my_df.query("game_length_seconds_max_600 >= 420")
 
-print(pro_df.groupby('game_length_seconds_max_600').game_number.nunique())
+dfs = []
+for game_number in pro_df.game_number.unique():
+    pro_df_one_game = pro_df.query(f'game_number == {game_number}')
+    print(pro_df_one_game)
 
-print(pro_df.groupby('opponent_race').game_number.nunique())
+    data_i = []
+    cumulative_count = []
+    for i in range(0, 601):
+        data_i.append(i)
+        cumulative_count.append(pro_df_one_game.query(f'inject_time < {i}').shape[0])
+    pro_data_cumulative_df = pd.DataFrame()
+    pro_data_cumulative_df['second'] = data_i
+    pro_data_cumulative_df['game_number'] = game_number
+    pro_data_cumulative_df['inject_count'] = cumulative_count
+    pro_data_cumulative_df['opponent_race'] = pro_df_one_game.iloc[0]['opponent_race']
+    dfs.append(pro_data_cumulative_df)
+pro_df = pd.concat(dfs, axis='index', ignore_index=True)
+local.write.csv(pro_df, '4_pro_inject_transform')
 
-print(pro_df)
 
-# Check just one game first
-pro_df_one_game = pro_df.query('game_number == 1')
-print(pro_df_one_game)
+dfs = []
+for game_number in my_df.game_number.unique():
+    my_df_one_game = my_df.query(f'game_number == {game_number}')
+    print(my_df_one_game)
 
-data_i = []
-cumulative_count = []
-for i in range(0, 601):
-    data_i.append(i)
-    cumulative_count.append(pro_df_one_game.query(f'inject_time < {i}').shape[0])
-pro_data_cumulative_df = pd.DataFrame()
-pro_data_cumulative_df['second'] = data_i
-pro_data_cumulative_df['inject_count'] = cumulative_count
-print(pro_data_cumulative_df)
+    data_i = []
+    cumulative_count = []
+    for i in range(0, 601):
+        data_i.append(i)
+        cumulative_count.append(my_df_one_game.query(f'inject_time < {i}').shape[0])
+    my_data_cumulative_df = pd.DataFrame()
+    my_data_cumulative_df['second'] = data_i
+    my_data_cumulative_df['game_number'] = game_number
+    my_data_cumulative_df['inject_count'] = cumulative_count
+    my_data_cumulative_df['opponent_race'] = my_df_one_game.iloc[0]['opponent_race']
+    dfs.append(my_data_cumulative_df)
+my_df = pd.concat(dfs, axis='index', ignore_index=True)
+local.write.csv(my_df, '4_my_inject_transform')
 
-# # Check the plots of all 3 oponent_race separately and combined first
-# terran_benchmarks
-# for game_number in pro_df.game_number.unique():
 
-plt.plot(pro_data_cumulative_df.second, pro_data_cumulative_df.inject_count)
-plt.savefig('../data/interim/4_pro_plot.png', dpi=300)
+# plt.plot(pro_data_cumulative_df.second, pro_data_cumulative_df.inject_count)
+# plt.savefig('../data/interim/4_pro_plot.png', dpi=300)
