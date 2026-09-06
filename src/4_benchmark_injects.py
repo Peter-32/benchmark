2b0685ca0a5e7a1db5cdfb3e31a122070e792a27
Peter-32 <peterjmyers1@gmail.com>
2026-09-06 10:54:46 -0600

    save

---
+++ src/4_benchmark_injects.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from utils import *
+pro_df = local.read.csv('2_extract_pro_injects', 'interim')
+
+# We will skip very short games for benchmarking analysis 
+pro_df = pro_df.query("game_length_seconds_max_600 >= 420")
+
+print(pro_df.groupby('game_length_seconds_max_600').game_number.nunique())
+
+print(pro_df.groupby('opponent_race').game_number.nunique())
+
+print(pro_df)
+
+# Check just one game first
+pro_df_one_game = pro_df.query('game_number == 1')
+print(pro_df_one_game)
+
+data_i = []
+cumulative_count = []
+for i in range(0, 601):
+    data_i.append(i)
+    cumulative_count.append(pro_df_one_game.query(f'inject_time < {i}').shape[0])
+pro_data_cumulative_df = pd.DataFrame()
+pro_data_cumulative_df['second'] = data_i
+pro_data_cumulative_df['inject_count'] = cumulative_count
+print(pro_data_cumulative_df)
+
+# # Check the plots of all 3 oponent_race separately and combined first
+# terran_benchmarks
+# for game_number in pro_df.game_number.unique():
