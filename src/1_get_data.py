a4babe449eba3b6c5fe3ff4bf0641dfef007f9fb
Peter-32 <peterjmyers1@gmail.com>
2026-09-05 21:48:00 -0600

    save

--- src/1_get_data.py
+++ src/1_get_data.py
@@ -1,4 +1,4 @@
 # Manually download from https://lotv.spawningtool.com/replays/?tag=644&order_by=play&p=1
 # If too many to download use Gem Quality AI's html tool to get it.  For now, manually downloading it.
 
-# For my data I just copy over from the computer.
+# For my data I just copy over from the computer.  To avoid overlapping file names, saved into multiple folders.
