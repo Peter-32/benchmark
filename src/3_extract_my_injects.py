ee8e147106ce259a0880a3c5618e63ea59320ed8
Peter-32 <peterjmyers1@gmail.com>
2026-09-06 14:18:19 -0600

    save

--- src/3_extract_my_injects.py
+++ src/3_extract_my_injects.py
@@ -6,7 +6,7 @@ from glob import glob
 
 all_dfs = []
 game_number = 0
-for account in ['account1', 'account2', 'account3']:
+for account in ['account3', 'account1', 'account2']:
     files = glob(f'../data/input/my_data/{account}/*')
     for file in tqdm.tqdm(files):
         
@@ -15,7 +15,7 @@ for account in ['account1', 'account2', 'account3']:
         except Exception as e:
             print(f"Failed to load {file}: {e}")
             continue
-        if replay.map_name in ['Emerald City CE', 'Reclamation LE', 'Fields of Death', 'Gemgarden LE', 'New Bed of Chaos LE', 'Rhoskallian LE', 'Rust Bucket LE', 'Sludge City', 'Undercurrent LE', 'Yellowjacket']:
+        if replay.map_name in ['Emerald City CE', 'Reclamation LE', 'Fields of Death', 'Gemgarden LE', 'New Bed of Chaos LE', 'Rhoskallian LE', 'Rust Bucket LE', 'Sludge City', 'Undercurrent LE', 'Yellowjacket', 'Phantom Mode']:
             continue
         game_number += 1
         assert replay.map_name in ['valid_maps', "At Eternity's Edge LE", 'Blackrock LE', 'Fear and Faith LE', 'Rainfall LE', 'Sanctuary III LE', 'Lockdown LE', 'Washout LE', 'Rorschach LE', 'Old Sun Temple LE'], replay.map_name
