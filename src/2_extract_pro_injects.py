77e8e9f1dd84edf063b92deaee29826342e1526c
Peter-32 <peterjmyers1@gmail.com>
2026-09-07 15:55:01 -0600

    save

--- src/2_extract_pro_injects.py
+++ src/2_extract_pro_injects.py
@@ -51,8 +51,13 @@ for file in tqdm.tqdm(files):
         except:
             pass
         
-        if 'SpawnLarva' in str(event.name):
+        if 'SpawnLarva' in str(event):
             inject_times_data.append(seconds)
+        elif event.name == 'TargetUnitCommandEvent':
+            if event.ability == None:
+                continue
+            if event.ability.name == 'SpawnLarva':
+                inject_times_data.append(seconds)
         if seconds > 10*60:
             break
     inject_times_df = pd.DataFrame()
--- src/3_extract_my_injects.py
+++ src/3_extract_my_injects.py
@@ -56,8 +56,13 @@ for account in ['sample']:
             except:
                 pass
         
-            if 'SpawnLarva' in str(event.name):
+            if 'SpawnLarva' in str(event):
                 inject_times_data.append(seconds)
+            elif event.name == 'TargetUnitCommandEvent':
+                if event.ability == None:
+                    continue
+                if event.ability.name == 'SpawnLarva':
+                    inject_times_data.append(seconds)
             if seconds > 10*60:
                 break
         inject_times_df = pd.DataFrame()
