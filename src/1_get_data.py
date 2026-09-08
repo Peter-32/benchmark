df3caf8c81631d4a4760ef3e5654dad6a86167b9
Peter-32 <peterjmyers1@gmail.com>
2026-09-07 21:21:02 -0600

    save

--- src/1_get_data.py
+++ src/1_get_data.py
@@ -1,4 +1,75 @@
-# Manually download from https://lotv.spawningtool.com/replays/?tag=644&order_by=play&p=1
-# If too many to download use Gem Quality AI's html tool to get it.  For now, manually downloading it.
+import sc2reader
+import pandas as pd
+import numpy as np
+import tqdm
+import sys
+from utils import *
+from glob import glob
 
-# For my data I just copy over from the computer.  To avoid overlapping file names, saved into multiple folders.
+path = os.path.join(sys.argv[1], '*')
+player_name = sys.argv[2]
+files = sorted(glob(path), key=os.path.getmtime, reverse=True)
+files_chosen = []
+found_zvz = False
+found_zvp = False
+found_zvt = False
+print(files, glob(path), path)
+for file in tqdm.tqdm(files):
+    try:
+        replay = sc2reader.load_replay(file, load_map=True)
+    except Exception as e:
+        print(f"Failed to load {file}: {e}")
+        continue
+    
+    if not replay.map_name in ["At Eternity's Edge LE", 'Blackrock LE', 'Fear and Faith LE', 'Rainfall LE', 'Sanctuary III LE', 'Lockdown LE', 'Washout LE', 'Rorschach LE', 'Old Sun Temple LE']:
+        continue
+
+    if not replay.release_string >= '5.0.16':
+        continue
+    
+    player1 = player_name
+    player2 = None
+    player1_won = None
+    player1_race = None
+    player2_race = None
+    for player in replay.players:
+        if player.name == player_name or player.name == 'Kairo':
+            player1_race = player.play_race
+            player1_won = player.result
+        else:
+            if player2 != None:
+                raise Exception(player.name, player2)
+            player2 = player.name
+            player2_race = player.play_race
+    # print(player1, player2, player1_race, player2_race)
+    if player1_race != 'Zerg':
+        continue
+    
+    for event in replay.events:
+        seconds = event.frame / 22.4
+        minutes = int(seconds // 60)
+        remaining_seconds = int(seconds % 60)
+        if seconds < 1:
+            continue
+    
+        try: 
+            if player2 in str(event):
+                continue           
+            if event.player == player2:
+                continue
+        except:
+            pass
+
+        if seconds > 10*60:
+            break
+    if seconds > 7*60:
+        if player2_race == 'Terran' and found_zvt == False:
+            files_chosen.append(file)
+        if player2_race == 'Zerg' and found_zvz == False:
+            files_chosen.append(file)
+        if player2_race == 'Protoss' and found_zvp == False:
+            files_chosen.append(file)
+    if len(files_chosen) >= 3:
+        break
+for file in files_chosen:
+    shutil.copy(file, '../data/input/my_data/sample')
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import subprocess
 import sys
 from glob import glob
+import tqdm
 
 all_files = glob("*.py")
-print([y for (x, y) in sorted([(str(x.split("_")[0]).zfill(2), x) for x in all_files]) if 'get_data' not in y and 'pro' not in y and y not in ['main.py', 'utils.py']])
-
-# subprocess.run([sys.executable, script_name])
+files_of_interest = [y for (x, y) in sorted([(str(x.split("_")[0]).zfill(2), x) for x in all_files]) if 'get_data' not in y and 'pro' not in y and y not in ['main.py', 'utils.py']]
+replay_path = r'C:\Users\peter\OneDrive\code\benchmark\data\input\my_data\sample_1'
+player_name = 'nemo'
+subprocess.run([sys.executable, '1_get_data.py', replay_path, player_name])
+for script_name in tqdm.tqdm(files_of_interest):
+    print(script_name)
+    subprocess.run([sys.executable, script_name])
